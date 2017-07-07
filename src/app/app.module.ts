753cb5feaa6a9d705901e2237f125f681d5d9b4e
prudywsh <antoineprudhomme5@gmail.com>
2017-07-07 20:07:38 +0200

    simple html page for login + unused pages removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -14,8 +13,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
@@ -26,8 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    LoginPage,
     HomePage,
     TabsPage
   ],
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-login',
+  templateUrl: 'login.html'
 })
-export class ContactPage {
+export class LoginPage {
 
   constructor(public navCtrl: NavController) {
 
