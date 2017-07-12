1b3db2d5ac7dfea58c22fd12c6007c3af70a9fa9
prudywsh <antoineprudhomme5@gmail.com>
2017-07-12 18:26:10 +0200

    if no token in the localStorage, redirect to login page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+import { AuthTokenStorageHelper } from '../../helpers/AuthTokenStorageHelper';
+
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [AuthTokenStorageHelper]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private authTokenStorageHelper: AuthTokenStorageHelper) {}
+
+  ngOnInit() {
+      // read the authentication token in the localStorage
+      const token = this.authTokenStorageHelper.getToken();
+
+      // if there is no token, redirect to login page
+      if (!token) {
+          this.navCtrl.push(LoginPage);
+      }
 
   }
 
