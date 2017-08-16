00a4d4aa63066c2ba8ba030fdaa8fdc2b291c8cd
prudywsh <antoineprudhomme5@gmail.com>
2017-08-16 21:13:54 +0200

    it doesn't work, but commit before try something else

--- src/pages/checkin/checkin.ts
+++ src/pages/checkin/checkin.ts
@@ -45,16 +45,36 @@ export class CheckinPage {
     }
 
     startScanner() {
-        // start scanning
+
+
+       // Optionally request the permission early
+this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+       console.log("okkkk");
+
        let scanSub = this.qrScanner.scan().subscribe((text: string) => {
          console.log('Scanned something', text);
 
          this.qrScanner.hide(); // hide camera preview
          scanSub.unsubscribe(); // stop scanning
-       });
+     }, err => console.log("scann err : ", err));
 
        // show camera preview
        this.qrScanner.show();
+
+       // wait for user to scan something, then the observable callback will be called
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
     }
 
 }
