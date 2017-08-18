b84ff720749c1fbb54f98e93441a8a8d60dbf3fd
prudywsh <antoineprudhomme5@gmail.com>
2017-08-18 23:28:31 +0200

    can compare 2 checkins and see the students that are only on one of the two chekins

--- src/pages/compareCheckins/compareCheckins.ts
+++ src/pages/compareCheckins/compareCheckins.ts
@@ -1,20 +1,80 @@
 import { Component } from '@angular/core';
 import { ViewController, NavParams } from 'ionic-angular';
 
+import { CheckinService } from '../../services/CheckinService';
+import { AuthStorageHelper } from '../../helpers/AuthStorageHelper';
+
 @Component({
-    templateUrl: 'compareCheckins.html'
+    templateUrl: 'compareCheckins.html',
+    providers: [CheckinService, AuthStorageHelper]
 })
 export class CompareCheckinsPage {
 
     first: any;
+    inFirstOnly: any;
     second: any;
+    inSecondOnly: any;
+    requestDone: boolean = false;
 
     constructor(
         public viewCtrl: ViewController,
-        params: NavParams
+        public params: NavParams,
+        private checkinService: CheckinService
     ) {
-        this.first = params.get('first');
-        this.second = params.get('second');
+        // get the 2 checkins
+        Promise.all([
+            new Promise((resolve, reject) => {
+                // get first the checkin
+                this.checkinService.get({id: params.get('first')})
+                    .subscribe(
+                        data => {
+                            this.first = JSON.parse(data._body);
+                            resolve();
+                        },
+                        err => reject(err)
+                    );
+            }),
+            new Promise((resolve, reject) => {
+                // get the second checkin
+                this.checkinService.get({id: params.get('second')})
+                    .subscribe(
+                        data => {
+                            this.second = JSON.parse(data._body);
+                            resolve();
+                        },
+                        err => reject(err)
+                    );
+            })
+        ])
+        .then(_ => {
+            this.compare();
+            this.requestDone = true;
+        })
+        .catch(err => console.log('err', err));
+    }
+
+    compare() {
+        // create two hash with the students's id
+        const first_hash = {};
+        const second_hash = {};
+
+        this.first.students.map(student => {
+            first_hash[student.id] = `${student.first_name} ${student.last_name}`;
+        });
+        this.second.students.map(student => {
+            second_hash[student.id] = `${student.first_name} ${student.last_name}`;
+        });
+
+        // loop on first_hash. If key is both on first_hash and second_hash, delete the key
+        for (let id in first_hash) {
+            if (second_hash[id]) {
+                delete first_hash[id];
+                delete second_hash[id];
+            }
+        }
+
+        this.inFirstOnly = Object.values(first_hash);
+        this.inSecondOnly = Object.values(second_hash);
     }
 
     dismiss() {
